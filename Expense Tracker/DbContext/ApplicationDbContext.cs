e8f79dcf7e65df13a4ca36537273570b68b76f5c
HusanjonDeveloper <muhammadaliyevhusanjon181@gmail.com>
2024-12-18 04:21:51 +0500

    Add  All File

--- Expense Tracker/DbContext/ApplicationDbContext.cs
+++ Expense Tracker/DbContext/ApplicationDbContext.cs
@@ -1,15 +1,14 @@
+using Expense_Tracker.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace Expense_Tracker.Models;
+namespace Expense_Tracker.DbContext;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
 {
-
     public ApplicationDbContext(DbContextOptions oprions) : base(oprions)
     { }
 
     public  DbSet<Transaction> Transactions { get; set; }
     public  DbSet<Category> Categories { get; set; }
-    
-    
+
 }
--- Expense Tracker/Program.cs
+++ Expense Tracker/Program.cs
@@ -1,4 +1,4 @@
-using Expense_Tracker.Models;
+using Expense_Tracker.DbContext;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,9 +6,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.UseNpgsql(builder.Configuration.GetConnectionString("TrackerDb"));
+    // for  PgAdmin 
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DevConnection"));
+    
+    // SQL Server 
+    
+   // options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    
 });
 var app = builder.Build();
 
@@ -20,16 +28,16 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
+app.MapStaticAssets();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
 
 app.Run();
-
